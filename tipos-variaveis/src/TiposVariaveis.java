4d16a38161da4d549feeaffacd9b617cd1a6f168
david oliveira <david.oliveira.mkt@gmail.com>
2024-07-19 23:00:52 -0300

    alteração 1

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+         //variaveis primitivas
+        
+         short numero = 1;  //usada para numeros curtos
+         int number = 1;    //variavel padrão para inteiros
+         float real = 1;    //para pequenos numeros reais
+         double fds = 2500;     // padrão para numeros reais grandes
+         String nome = "meu nome";  //para caracteres 
+         
     }
 }
