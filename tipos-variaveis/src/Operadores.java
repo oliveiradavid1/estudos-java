03ae64a7e1cfef31881c4919a7610f8f86e96c5b
david oliveira <david.oliveira.mkt@gmail.com>
2024-07-20 20:26:24 -0300

    alteração metodos

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,8 +1,27 @@
 public class Operadores {
 
     public static void main(String[] args) {
+        int a, b;
+        a = 1;
+        b = 2;
 
+        int qualquer = 5;
+
+        System.out.println(- qualquer);
+
+        System.out.println(qualquer);
+//incremento
+        System.out.println(++ qualquer);
+
+        System.out.println(qualquer);
+//condição ternaria
+
+        String resultado = a==b ? "verdadeiro" : "falso";
+
+        System.out.println(resultado);
+       
         
+
         
     }
 
---
+++ tipos-variaveis/src/metodos.java
@@ -0,0 +1,10 @@
+public class metodos {
+
+    public static void main(String[] args) {
+        
+// deve ser nomeado com serbos
+//seguir o padrao camelcase
+
+
+    }
+}
