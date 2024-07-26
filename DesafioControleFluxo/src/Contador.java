15d0e1a01722d9ca11c3008c149d13a77b0082fa
david oliveira <david.oliveira.mkt@gmail.com>
2024-07-25 22:36:28 -0300

    commit1

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,37 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Contador {
+
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("DIGITE O PRIMEIRO PARAMETRO :");
+        int parametroUm = scanner.nextInt();
+    
+        System.out.println("DIGITE O PRIMEIRO PARAMETRO :");
+        int parametroDois = scanner.nextInt();
+        
+        try{
+        contar (parametroUm, parametroDois);
+
+        } catch (ParametrosInvalidosExeption e){
+
+            System.out.println("o segundo parâmetro precisa ser maior que o primeiro ! ");
+
+        }
+        
+    }
+    public static void contar (int parametroUm, int parametroDois ) throws ParametrosInvalidosExeption {
+
+        if( parametroUm > parametroDois ){
+           throw new ParametrosInvalidosExeption();
+        }
+      
+        for( int i =1 ; i <= (parametroDois - parametroUm) ; i ++ ){
+            System.out.println("IMPRIMINDO NUMERO " + i );
+        }
+    }
+    
+}
---
+++ DesafioControleFluxo/src/ParametrosInvalidosExeption.java
@@ -0,0 +1,7 @@
+public class ParametrosInvalidosExeption extends Exception{
+    
+    public static void main(String[] args) {
+        
+        System.out.println("o segundo parâmetro precisa ser maior que o primeiro ! ");
+    }
+}
